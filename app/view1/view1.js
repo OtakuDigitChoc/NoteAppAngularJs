b5da68bf4fce70ff9dc6fb7ac7464443facc8793
OtakuDigitChoc <abdelaliboulajine@gmail.com>
2016-01-06 01:02:54 +0100

    l'application permet d'ajouter et de supprimer des notes

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -3,6 +3,20 @@
 angular.module('myApp.view1', [])
 
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', ['$scope',function($scope) {
+    $scope.notes = [];
 
+    $scope.addNote = function (){
+      console.log($scope.notes);
+      $scope.notes.push({
+        id: $scope.notes.length,
+        content: $scope.notes.content
+      });
+      $scope.notes.content ='';
+    }
+
+    $scope.sup = function (id) {
+      console.log(id);
+                $scope.notes.splice(id,1);
+    }
 }]);
